72d28668ddd392b637a198b914e81e979df0ca26
Shridevi-0109 <shriathi01@gmail.com>
2023-03-16 18:03:41 +0530

    Cucumber casestudy

--- src/test/java/stepsDefs/DemoBlazeStepDef.java
+++ src/test/java/stepsDefs/DemoBlazeStepDef.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -12,118 +14,127 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
+import io.cucumber.java.After;
+import io.cucumber.java.AfterAll;
 import io.cucumber.java.BeforeAll;
-import io.cucumber.java.en.*;
+import io.cucumber.java.Scenario;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DemoBlazeStepDef {
 	static WebDriver driver;
 	static WebDriverWait wait;
-	int itemSize;
-
+	String search,beforePrice,afterPrice;
+	WebElement rate;
+	List<WebElement> items;
 	@BeforeAll
 	public static void setup() {
 		WebDriverManager.edgedriver().setup();
-		driver = new EdgeDriver();
+		driver=new EdgeDriver();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(50));
-		wait = new WebDriverWait(driver, Duration.ofSeconds(40));
+		driver.manage().timeouts().implicitlyWait(Duration.ofMinutes(1));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofMinutes(1));
+		wait=new WebDriverWait(driver,Duration.ofMinutes(1));
+		driver.get("https://www.demoblaze.com/");
 	}
-
-	@Given("User is on Launch Page")
-	public void user_is_on_launch_page() {
-		driver.get("https://www.demoblaze.com/index.html");
+	@Given("User into the login page")
+	public void user_into_the_login_page() {
+		driver.findElement(By.xpath("//a[contains(text(),'Log in')]")).click();
 	}
-
-	@When("User navigates to the login Page enters {string} and {string}")
-	public void user_navigates_to_the_login_page_enters_and(String userName, String password) {
-		driver.findElement(By.id("login2")).click();
-		wait.until(ExpectedConditions.elementToBeClickable(By.id("loginusername")));
-		driver.findElement(By.id("loginusername")).sendKeys(userName);
-		driver.findElement(By.id("loginpassword")).sendKeys(password);
-		driver.findElement(By.cssSelector("button[onclick=\"logIn()\"]")).click();
+	@When("User enters login credientials")
+	public void user_enters_login_credientials() {
+		driver.findElement(By.xpath("//input[@id='loginusername']")).sendKeys("ShriAthi");
+		driver.findElement(By.xpath("//input[@id='loginpassword']")).sendKeys("Shri.01");
+		driver.findElement(By.xpath("//button[contains(text(),'Log in')]")).click();
 	}
-	
-	@Then("It should display Home Page")
-	public void it_should_display_home_page() {
-		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[contains(text(),'Welcome')]")));
-		WebElement welcomeTxt = driver.findElement(By.xpath("//a[contains(text(),'Welcome')]"));
-		Assert.assertEquals(welcomeTxt.getText(), "Welcome ShriAthi");
+	@Then("Should Display Home Page")
+	public void should_display_home_page() {
+		Assert.assertEquals(driver.findElement(By.xpath("//a[contains(text(),'Welcome ShriAthi')]")).getText(), "Welcome ShriAthi");
 	}
-
-	@When("User add an {string} to the cart")
-	public void user_add_an_to_the_cart(String items) {
-		WebElement product = driver.findElement(By.partialLinkText(items));
-		wait.until(ExpectedConditions.elementToBeClickable(product));
-		product.click();
-		driver.findElement(By.xpath("//a[contains(text(),'Add to')]")).click();
+	@When("Add an Item {string} to {string} Cart")
+	public void add_an_item_to_cart(String catagory, String item) {
+		driver.findElement(By.xpath("//a[contains(text(),'Home')]")).click();
+		String catagoryPath="//a[text()='"+catagory+"']";
+		driver.findElement(By.xpath(catagoryPath)).click();
+		String searchPath="//a[text()='"+item+"']";
+		driver.findElement(By.xpath(searchPath)).click();
+		driver.findElement(By.xpath("//a[contains(text(),'Add to cart')]")).click();
 		wait.until(ExpectedConditions.alertIsPresent());
-		Alert alert = driver.switchTo().alert();
+		Alert alert=driver.switchTo().alert();
 		alert.accept();
-		driver.findElement(By.xpath("(//ul/li//a)[1]")).click();
-	}
+		search=item;
 
-	@Then("the items added to the cart")
-	public void the_items_added_to_the_cart() {
-		driver.findElement(By.xpath("//a[text()='Cart']")).click();
-		List<WebElement> products = driver.findElements(By.xpath("//tr/td[2]"));
-		wait.until(ExpectedConditions.visibilityOfAllElements(products));
-		itemSize = products.size();
-		boolean proAdded = true;
-		if(itemSize>1) {
-			Assert.assertTrue(proAdded);
+	}
+	@Then("Items must be added to cart")
+	public void items_must_be_added_to_cart() {
+		driver.findElement(By.xpath("//a[contains(text(),'Cart')]")).click();
+		List<WebElement> items=driver.findElements(By.xpath("//td[2]"));
+		boolean flag=false;
+		for(WebElement test:items) {
+			if(test.getText().equalsIgnoreCase(search)) {
+				Assert.assertEquals(test.getText(),search);
+				flag=true;
+			}
+		}
+		Assert.assertTrue(flag);
+	}
+	@When("List of items should be available in Cart")
+	public void list_of_items_should_be_available_in_cart() {
+		driver.findElement(By.xpath("//a[contains(text(),'Cart')]")).click();
+		List<WebElement> items=driver.findElements(By.xpath("//tbody/tr"));
+		Assert.assertTrue(items.size()!=0);
+		rate=driver.findElement(By.id("totalp"));
+		beforePrice=rate.getText();
+		System.out.println(beforePrice);
+	}
+	@Then("Delete an item from cart")
+	public void delete_an_item_from_cart() throws InterruptedException {
+		driver.findElement(By.xpath("(//td[4]//a)[1]")).click();
+		Thread.sleep(3000);
+		rate=driver.findElement(By.id("totalp"));
+		afterPrice=rate.getText();
+		System.out.println(afterPrice);
+		Assert.assertNotEquals(beforePrice, afterPrice);
+	}
+	@When("Items Should be available in Cart")
+	public void items_should_be_available_in_cart() {
+		driver.findElement(By.xpath("//a[contains(text(),'Cart')]")).click();
+		items=driver.findElements(By.xpath("//tbody/tr"));
+		Assert.assertTrue(items.size()!=0);
+	}
+	@Then("Place order")
+	public void place_order() {
+		if(items.size()!=0) {
+			driver.findElement(By.xpath("//button[contains(text(),'Place Order' )]")).click();
+			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'Purchase')]")));
+			driver.findElement(By.xpath("//input[@id='name']")).sendKeys("ShriAthi");
+			driver.findElement(By.xpath("//input[@id='country']")).sendKeys("India");
+			driver.findElement(By.xpath("//input[@id='city']")).sendKeys("Tirunelveli");
+			driver.findElement(By.xpath("//input[@id='card']")).sendKeys("1234 4567 7890 0123");
+			driver.findElement(By.xpath("//input[@id='month']")).sendKeys("September");
+			driver.findElement(By.xpath("//input[@id='year']")).sendKeys("2023");
 		}
 	}
-
-	@Given("User is on Cart Page")
-	public void user_is_on_cart_page() {
-		List<WebElement> listOfpro = driver.findElements(By.xpath("//td"));
-		boolean list = listOfpro.isEmpty();
-		boolean productsAvail = true;
-		if(!list) {
-			Assert.assertTrue(productsAvail);
-		}
-	}
-
-	@When("User deletes an item from the cart page")
-	public void user_deletes_an_item_from_the_cart_page() throws InterruptedException {
-		driver.findElement(By.xpath("//td/a[1]")).click();
-		Thread.sleep(2000);
-	}
-
-	@Then("the item must be deleted from the cart page")
-	public void the_item_must_be_deleted_from_the_cart_page() {
-		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//td")));
-		List<WebElement> currentproduct = driver.findElements(By.xpath("//tr/td[2]"));
-		int cntproductSize = currentproduct.size();
-		boolean productDeleted = true;
-		if(itemSize>cntproductSize) {
-			Assert.assertTrue(productDeleted);
-		}
-	}
-
-	@When("User places an order")
-	public void user_places_an_order() throws InterruptedException {
-		WebElement placeOrdBtn = driver.findElement(By.xpath("//button[text()='Place Order']"));
-		wait.until(ExpectedConditions.visibilityOf(placeOrdBtn));
-		placeOrdBtn.click();
-		driver.findElement(By.id("name")).sendKeys("ShriAthi");
-		driver.findElement(By.id("country")).sendKeys("India");
-		driver.findElement(By.id("city")).sendKeys("Chennai");
-		driver.findElement(By.id("card")).sendKeys("1234 4567 7890 0123");
-		driver.findElement(By.id("month")).sendKeys("September");
-		driver.findElement(By.id("year")).sendKeys("2023");
-		driver.findElement(By.xpath("//button[text()='Purchase']")).click();
-	}
-
-	@Then("the item must be placed")
-	public void the_item_must_be_placed() throws InterruptedException {
-		WebElement successMsg = driver.findElement(By.xpath("//h2[text()='Thank you for your purchase!']"));
-		wait.until(ExpectedConditions.visibilityOf(successMsg));
-		Assert.assertEquals(successMsg.getText(), "Thank you for your purchase!");
-		Thread.sleep(2000);
+	@Then("Purchase Items")
+	public void purchase_items() throws InterruptedException {
+		driver.findElement(By.xpath("//button[contains(text(),'Purchase')]")).click();
+		Assert.assertEquals(driver.findElement(By.xpath("(//h2)[3]")).getText(), "Thank you for your purchase!");
+		Thread.sleep(1000);
 		driver.findElement(By.xpath("//button[contains(text(),'OK')]")).click();
 	}
+	@After
+	public void attachImgToReport(Scenario scenario) {
+		TakesScreenshot scr=(TakesScreenshot)driver;
+		byte[] img=scr.getScreenshotAs(OutputType.BYTES);
+		scenario.attach(img, "image/png", "imageOne");
+		
+	}
+	@AfterAll
+	public static void finish() {
+		driver.close();
+	}
+
 
 }
--- src/test/resources/features/demoBlazeBDD.feature
+++ src/test/resources/features/demoBlazeBDD.feature
@@ -1,27 +1,23 @@
-Feature: DemoBlaze Web Tests
+Feature: Product Store Page Test
 
-Scenario: Login into an Application
-Given User is on Launch Page
-When User navigates to the login Page enters "ShriAthi" and "Shri.01"
-Then It should display Home Page
+Scenario: Login into App
+Given User into the login page
+When User enters login credientials
+Then Should Display Home Page
 
-Scenario Outline: Add items to cart
-Given User is on Launch Page
-When User add an "<items>" to the cart
-Then the items added to the cart
-Examples:
-	| items |
-	| Nexus 6 |
-	| HTC One M9 |
-	| Sony vaio i7 |
-	| Samsung galaxy s7 |
+Scenario Outline: Add Items To Cart
+When Add an Item "<catagory>" to "<item>" Cart
+Then Items must be added to cart
+Examples: 
+	|catagory|item|
+	|Laptops|MacBook air|
+	|Phones|Samsung galaxy s6|
 	
-Scenario: Delete an item
-Given User is on Cart Page
-When User deletes an item from the cart page
-Then the item must be deleted from the cart page
+Scenario: Delete an Item
+When List of items should be available in Cart
+Then Delete an item from cart
 
-Scenario: Place an order
-Given User is on Cart Page
-When User places an order
-Then the item must be placed
+Scenario: Place Order
+When Items Should be available in Cart
+Then Place order
+And Purchase Items
